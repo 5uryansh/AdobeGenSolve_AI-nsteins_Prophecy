b109f565b35a622dfafa41ed3b6334e59c582fb2
ojhapiyush <ojhapiyush063@gmail.com>
2024-08-11 11:50:11 +0530

    Added evaluation file

---
+++ evaluation.py
@@ -0,0 +1,40 @@
+import svgwrite
+import cairosvg
+import numpy as np
+
+def polylines2svg(paths_XYs, svg_path):
+    W, H = 0, 0
+    for path_XYs in paths_XYs:
+        for XY in path_XYs:
+            W, H = max(W, np.max(XY[:, 0])), max(H, np.max(XY[:, 1]))
+
+    padding = 0.1
+    W, H = int(W + padding * W), int(H + padding * H)
+
+    # Create a new SVG drawing
+    dwg = svgwrite.Drawing(svg_path, profile='tiny', shape_rendering='crispEdges')
+    group = dwg.g()
+
+    colours = ['red', 'green', 'blue', 'orange', 'purple', 'brown']  # Example colors list
+
+    for i, path in enumerate(paths_XYs):
+        path_data = []
+        c = colours[i % len(colours)]
+        for XY in path:
+            path_data.append(("M", (XY[0, 0], XY[0, 1])))
+            for j in range(1, len(XY)):
+                path_data.append(("L", (XY[j, 0], XY[j, 1])))
+            if not np.allclose(XY[0], XY[-1]):
+                path_data.append(("Z", None))
+        group.add(dwg.path(d=path_data, fill=c, stroke='none', stroke_width=2))
+
+    dwg.add(group)
+    dwg.save()
+
+    png_path = svg_path.replace('.svg', '.png')
+    fact = max(1, 1024 // min(H, W))
+    cairosvg.svg2png(url=svg_path, write_to=png_path, parent_width=W, parent_height=H,
+                     output_width=fact * W, output_height=fact * H,
+                     background_color='white')
+
+    return
